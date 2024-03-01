cbc2422bd63fcf3f2d2de382a4b97e0cbc69d6ef
Japheth Russell <japheth.russell@idealabsoftware.com>
2024-03-01 13:30:41 +0100

    Fix permission issue

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 docker compose up -d
 
 docker exec bubble-gum bash -c "php spark migrate"
-sudo chmod 777 ./writable -R # Needed on some systems to fix permission errors [NB: don't use 777 on an actual application 🫡]
+
+sudo chmod 777 ./basket/writable -R # Needed on some systems to fix permission errors [NB: don't use 777 on an actual application 🫡]
 
 sleep 60 # Wait for rabbitmq to start up
 
