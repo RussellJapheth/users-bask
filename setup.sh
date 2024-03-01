bfe09f69e5c22a2c2ce0cea949a01d856376f917
Japheth Russell <japheth.russell@idealabsoftware.com>
2024-03-01 12:47:55 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@ cd basket
 
 cp env .env
 
-docker exec bubble-gum bash -c "composer install"
 
 chmod 777 ./writable -R # Needed on some systems to fix permission errors [NB: don't use 777 on an actual application 🫡]
 
 cd ..
 
 docker compose up -d --build
+
+docker exec bubble-gum bash -c "composer install"
 
 docker exec bubble-gum bash -c "php spark migrate"
 
